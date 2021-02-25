041a86d42c2b807fd99d910763dd5c8470c153e4
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-02-25 16:24:26 -0500

    2-25-2021_4:25PMEST

--- EmployeeSQL/QuickDBD-Schema export.sql
+++ EmployeeSQL/QuickDBD-Schema export.sql
@@ -1,4 +1,4 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
@@ -49,11 +49,8 @@ CREATE TABLE "dept_manager" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" TEXT   NOT NULL,
-
+    "dept_no" TEXT   NOT NULL
 );
-
-
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "salaries" ("emp_no");
@@ -72,53 +69,4 @@ REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
-
-
---1. List the following details of each employee: employee number, last name, first name, sex, and salary.
-
-select a.emp_no, a.last_name, a.first_name, a.sex, b.salary 
-from employees a
-join salaries b
-	on (a.emp_no = b.emp_no)
-;
-
---2. List first name, last name, and hire date for employees who were hired in 1986.
-
-select first_name, last_name, hire_date
-from employees
-where date_part('year', hire_date) = '1986'
-;
-
---3. List the manager of each department with the following information: department number, department name, 
---the manager's employee number, last name, first name.
-
-select a.dept_no, b.dept_name, a.emp_no, c.last_name, c.first_name 
-from dept_manager a
-join departments b
-	on (a.dept_no = b.dept_no)
-join employees c
-	on (a.emp_no = c.emp_no)
-;
-
---4. List the department of each employee with the following information: employee number, last name, 
---first name, and department name.
-
-select a.emp_no, c.last_name, c.first_name, b.dept_name  
-from dept_emp a
-join departments b
-	on (a.dept_no = b.dept_no)
-join employees c
-	on (a.emp_no = c.emp_no)
-;
-
---5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
-select first_name, last_name
-from employees
-where first_name = 'Hercules'
-and last_name like 'B%'	
-;
-
---6. List all employees in the Sales department, including their employee number, 
---last name, first name, and department name.
 
