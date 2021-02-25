86aef879fbc89881eb8dd7942d8ba49cac98b05a
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-02-25 16:37:24 -0500

    2-25-2021_4:37PMEST

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -76,10 +76,10 @@ REFERENCES "departments" ("dept_no");
 
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
-select a.emp_no, a.last_name, a.first_name, a.sex, b.salary 
-from employees a
-join salaries b
-	on (a.emp_no = b.emp_no)
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary 
+from employees e
+join salaries s
+	on (e.emp_no = s.emp_no)
 ;
 
 --2. List first name, last name, and hire date for employees who were hired in 1986.
@@ -92,23 +92,23 @@ where date_part('year', hire_date) = '1986'
 --3. List the manager of each department with the following information: department number, department name, 
 --the manager's employee number, last name, first name.
 
-select a.dept_no, b.dept_name, a.emp_no, c.last_name, c.first_name 
-from dept_manager a
+select d.dept_no, b.dept_name, d.emp_no, e.last_name, e.first_name 
+from dept_manager d
 join departments b
-	on (a.dept_no = b.dept_no)
-join employees c
-	on (a.emp_no = c.emp_no)
+	on (d.dept_no = b.dept_no)
+join employees e
+	on (d.emp_no = e.emp_no)
 ;
 
 --4. List the department of each employee with the following information: employee number, last name, 
 --first name, and department name.
 
-select a.emp_no, c.last_name, c.first_name, b.dept_name  
-from dept_emp a
+select d.emp_no, e.last_name, e.first_name, b.dept_name  
+from dept_emp d
 join departments b
-	on (a.dept_no = b.dept_no)
-join employees c
-	on (a.emp_no = c.emp_no)
+	on (d.dept_no = b.dept_no)
+join employees e
+	on (d.emp_no = e.emp_no)
 ;
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
@@ -122,3 +122,18 @@ and last_name like 'B%'
 --6. List all employees in the Sales department, including their employee number, 
 --last name, first name, and department name.
 
+select d.emp_no, e.last_name, e.first_name, c.dept_name
+from dept_emp d
+join employees e
+	on (d.emp_no = e.emp_no)
+join departments c
+	on (d.dept_no = c.dept_no)
+where d.dept_no in (
+	select dept_no 
+	from departments 
+	where dept_name = 'Sales'
+	)
+;
+
+--7. List all employees in the Sales and Development departments, including their employee number, 
+--last name, first name, and department name.
