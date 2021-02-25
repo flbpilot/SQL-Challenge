d3b2dc2c718fdf0dc5b9447a477834144e1c82a8
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-02-25 15:49:29 -0500

    2-25-2021_3:49PMEST

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -1,5 +1,5 @@
---1. list all employees and display info:  
-	-- employee number, last name, first name, sex, and salary.
+
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
 select a.emp_no, a.last_name, a.first_name, a.sex, b.salary 
 from employees a
@@ -7,3 +7,19 @@ join salaries b
 	on (a.emp_no = b.emp_no)
 ;
 
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+
+select first_name, last_name, hire_date
+from employees
+where date_part('year', hire_date) = '1986'
+;
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+select a.dept_no, b.dept_name, a.emp_no, c.last_name, c.first_name 
+from dept_manager a
+join departments b
+	on (a.dept_no = b.dept_no)
+join employees c
+	on (a.emp_no = c.emp_no)
+;
