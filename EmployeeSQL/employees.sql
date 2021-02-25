e538d6abe94c4a2b72ebf48fb724ef26034dbf5d
Flbpilot <68716202+flbpilot@users.noreply.github.com>
2021-02-25 15:51:08 -0500

    2-25-2021_3:50PMEST

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -14,7 +14,8 @@ from employees
 where date_part('year', hire_date) = '1986'
 ;
 
---3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+--3. List the manager of each department with the following information: department number, department name, 
+--the manager's employee number, last name, first name.
 
 select a.dept_no, b.dept_name, a.emp_no, c.last_name, c.first_name 
 from dept_manager a
@@ -23,3 +24,6 @@ join departments b
 join employees c
 	on (a.emp_no = c.emp_no)
 ;
+
+--4. List the department of each employee with the following information: employee number, last name, 
+--first name, and department name.
